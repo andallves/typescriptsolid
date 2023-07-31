4de267a12ff3ebf7b0f39ba1df44e85bcd1c6e3d
Andre Alves <andrealves10a@gmail.com>
2023-07-31 12:58:01 -0300

    Legacy

---
+++ src/legacy/shopping-cart-legacy.ts
@@ -0,0 +1,65 @@
+type CartItem = { name: string; price: number };
+type OrderStatus = 'open' | 'closed';
+
+export class ShoppingCartLegacy {
+  private readonly _items: CartItem[] = [];
+  private _orderStatus: OrderStatus = 'open';
+
+  addItem(item: CartItem): void {
+    this._items.push(item);
+  }
+
+  removeItem(index: number): void {
+    this._items.splice(index, 1);
+  }
+
+  get items(): Readonly<CartItem[]> {
+    return this._items;
+  }
+
+  get orderStatus(): OrderStatus {
+    return this._orderStatus;
+  }
+
+  total(): number {
+    return +this._items
+      .reduce((total, next) => total + next.price, 0)
+      .toFixed(2);
+  }
+
+  checkout(): void {
+    if (this.isEmpty()) {
+      console.log('Seu carrinho está vazio');
+    }
+    this._orderStatus = 'closed';
+    this.sendMessage(`Seu pedido com total de ${this.total()} foi recebido.`);
+    this.saveOrder();
+    this.clear();
+  }
+
+  isEmpty(): boolean {
+    return this._items.length === 0;
+  }
+
+  sendMessage(msg: string): void {
+    console.log(msg);
+  }
+
+  saveOrder(): void {
+    console.log('Pedido salvo com sucesso...');
+  }
+
+  clear(): void {
+    console.log('Carrinho de comprar foi limpo');
+    this._items.length = 0;
+  }
+}
+
+const shoppingCart = new ShoppingCartLegacy();
+shoppingCart.addItem({ name: 'Mochila', price: 89.9 });
+shoppingCart.addItem({ name: 'Caderno', price: 19.9 });
+shoppingCart.addItem({ name: 'Caneta', price: 2.9 });
+
+console.log(shoppingCart.items);
+console.log(shoppingCart.total());
+shoppingCart.checkout();
