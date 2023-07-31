9acb79fb21fc6f43a5995cb7d33de2eb529d3d5c
Andre Alves <andrealves10a@gmail.com>
2023-07-31 12:58:20 -0300

    Open/closed Principle

---
+++ src/ocp/main.ts
@@ -0,0 +1,29 @@
+/*
+  OCP - Open/closed principle
+  Entidade devem estar abertas para extensão, mas fechadas para modicação.
+*/
+
+import { Messaging } from './services/messaging';
+import { Order } from './classes/order';
+import { Persistency } from './services/persistency';
+import { Product } from './classes/product';
+import { ShoppingCart } from './classes/shopping-cart';
+import { TenPorcentDiscount } from './classes/discount';
+
+// const fiftyPorcentDiscount = new FiftyPorcentDiscount();
+const tenPorcentDiscount = new TenPorcentDiscount();
+const shoppingCart = new ShoppingCart(tenPorcentDiscount);
+const messaging = new Messaging();
+const persistency = new Persistency();
+const order = new Order(shoppingCart, messaging, persistency);
+
+shoppingCart.addItem(new Product('Mochila', 89.9));
+shoppingCart.addItem(new Product('Caderno', 19.9));
+shoppingCart.addItem(new Product('Caneta', 2.9));
+
+console.log(shoppingCart.items);
+console.log(shoppingCart.total());
+console.log(shoppingCart.totalWithDiscount());
+console.log(order.orderStatus);
+order.checkout();
+console.log(order.orderStatus);
